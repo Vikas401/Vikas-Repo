9abaa2ff00eb3046235006f64e83d41ab95e2251
APPX01 <bhushan.patil@appxtechnologies.com>
2019-12-02 10:58:23 +0530

    adding header componenr and link to home , shop page and contcact

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
-import HomePage from './pages/homepage.component';
-import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
-
+import Header from './componet/header/header.component';
+import HomePage from './pages/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
    <div>
+   <Header/>
+   <Switch>
    <Route exact path='/' component={HomePage}/>
    <Route path='/shop' component={ShopPage}/>
+   </Switch>
    </div>
     
   );
