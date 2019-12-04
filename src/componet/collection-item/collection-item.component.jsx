cbd6a23b66933b9e971199c9b21e48eba86ed19c
APPX01 <bhushan.patil@appxtechnologies.com>
2019-12-04 13:04:14 +0530

    updated shop data with new data, updated cart untils to group items in cart reducer

--- src/componet/collection-item/collection-item.component.jsx
+++ src/componet/collection-item/collection-item.component.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
+import CustomButton from '../custom-button/custom-button.component';
+
+import { addItems } from '../../redux/cart/cart.action';
+
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ item, addItems }) => {
+    const { name, price, imageUrl} = item;
+    return (
     <div className='collection-item'>
     <div
     className='image'
@@ -14,9 +22,15 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
     <span className='name'>{ name }</span>
     <span className='price'>{ price }</span>
     </div>
-    
+    <CustomButton onClick={() => addItems(item)} inverted> 
+    Add To Cart
+    </CustomButton>
     
     </div>
-);
+)};
+  const mapDispatchToProps = dispatch => ({
+      addItems: item => dispatch(addItems(item))
+  })
 
-export default CollectionItem;
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
